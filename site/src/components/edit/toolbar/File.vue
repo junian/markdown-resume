5e0e13d0939d2376ea87caf57a4593ee360b2b6e
Junian Triajianto <author@junian.dev>
2026-02-15 09:01:41 +0700

    feat(toolbar): Add Iconify script to HTML export

--- site/src/components/edit/toolbar/File.vue
+++ site/src/components/edit/toolbar/File.vue
@@ -103,12 +103,14 @@ const exportHtml = () => {
   const fullCss = data.cssContent + dynamicCss;
   
   // Create complete HTML document
+  const scriptTag = '<script src="https://code.iconify.design/3/3.1.0/iconify.min.js"><\/script>';
   const htmlDocument = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${data.curResumeName}</title>
+  ${scriptTag}
   <style>
 ${fullCss}
   </style>
