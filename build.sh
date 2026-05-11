f208349c5d2e8427048300fcd802f12e8ba3271d
Junian Triajianto <author@junian.dev>
2026-05-11 21:46:28 +0700

    chore(build): Comment out redirect configuration in build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 pnpm install --frozen-lockfile
 pnpm release
 
-echo "https://markdown-resume-app.netlify.app/ https://www.junian.dev/markdown-resume/ 301" > site/dist/_redirects
-echo "https://markdown-resume-app.netlify.app/* https://www.junian.dev/markdown-resume/:splat 301" >> site/dist/_redirects
+# echo "https://markdown-resume-app.netlify.app/ https://www.junian.dev/markdown-resume/ 301" > site/dist/_redirects
+# echo "https://markdown-resume-app.netlify.app/* https://www.junian.dev/markdown-resume/:splat 301" >> site/dist/_redirects
