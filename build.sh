2cd1c76c1d6095da7aa59d0fbae7b8cbd8bb71dc
Junian Triajianto <author@junian.dev>
2026-05-11 21:38:39 +0700

    chore: Add build script for deployment automation

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pnpm install --frozen-lockfile
+pnpm release
+
+echo "https://markdown-resume-app.netlify.app/ https://www.junian.dev/markdown-resume/ 301" > site/dist/_redirects
+echo "https://markdown-resume-app.netlify.app/* https://www.junian.dev/markdown-resume/:splat 301" >> site/dist/_redirects
