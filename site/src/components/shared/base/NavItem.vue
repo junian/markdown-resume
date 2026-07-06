f71a5d4a278ddb0146edf9a952d577164e987233
Junian Triajianto <author@junian.dev>
2026-07-06 11:53:18 +0700

    style(nav): Enhance NavItem with hover effects and padding

--- site/src/components/shared/base/NavItem.vue
+++ site/src/components/shared/base/NavItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <nuxt-link class="hstack cursor-pointer space-x-1" :to="link">
+  <nuxt-link class="hstack cursor-pointer space-x-1 p-1.5 rounded transition-colors hover:bg-gray-200 dark:hover:bg-gray-700" :to="link">
     <span v-if="icon" :class="icon" md:text-lg />
     <span class="truncate max-w-xs hide-on-mobile">
       {{ label }}
