19ca450d2af5132e5c637925cd264dc1e9b6baef
Junian Triajianto <author@junian.dev>
2026-07-22 13:09:35 +0700

    feat(resume): add default resume markdown content as a separate file

--- site/src/utils/constants/default.ts
+++ site/src/utils/constants/default.ts
@@ -1,4 +1,5 @@
 import type { ResumeStyles } from "~/types";
+import defaultResumeMarkdown from "~/assets/default-resume.md?raw";
 import { SYSTEM_CJK_FONT_FAMILY } from "./data";
 
 export const DEFAULT_NAME = "My Resume";
@@ -20,110 +21,7 @@ export const DEFAULT_STYLES = {
   paper: "A4"
 } as ResumeStyles;
 
-export const DEFAULT_MD_CONTENT = `---
----
-
-# Bruce Wayne
-
-<iconify-icon icon="eva:people-outline"></iconify-icon> [example.com](https://example.com/)
-  : <iconify-icon icon="tabler:brand-github"></iconify-icon> [github.com/example](https://github.com/example)
-  : <iconify-icon icon="tabler:phone"></iconify-icon> [(+1) 123-456-7890](https://wa.me/11234567890)
-
-<iconify-icon icon="ic:outline-location-on"></iconify-icon> 1234 Abc Street, Example, EX 01234
-  : <iconify-icon icon="tabler:brand-linkedin"></iconify-icon> [linkedin.com/in/example](https://linkedin.com/in/example/)
-  : <iconify-icon icon="tabler:mail"></iconify-icon> [email@example.com](mailto:mail@example.com)
-
-## Experience
-
-**Machine Learning Engineer Intern**
-  : **Slow Feet Technology**
-  : **Jul 2021 - Present**
-
-- Devised a new food-agnostic formulation for fine-grained cross-ingredient meal cooking and subsumed the recent popular works into the proposed scheme
-- Proposed a cream of mushroom soup recipe which is competitive when compared with the SOTA recipes with complex steps by only altering the way of cutting mushroom, published in NeurIPS 2099 (see [~P1])
-- Developed a pan for meal cooking which is benefiting the group members' research work
-
-
-**Reseach Intern**
-  : **Paddling University**
-  : **Aug 2020 - Present**
-
-- Designed an efficient method for mapo tofu quality estimation via thermometer
-- Proposed a fast stir frying algorithm for tofu cooking problems, which specifies the amount of the hot sauce instead of using terms like "as much as you can", published in CVPR 2077 (see [~P2])
-- Outperformed SOTA methods while cooking much more efficient in experiments on popular tofu
-
-
-**Research Assistant**
-  : **Huangdu Institute of Technology**
-  : **Mar 2020 - Jun 2020**
-
-- Proposed a novel framework consisting of a spoon and a pair of chopsticks for eating mapo toufu
-- Designed a tofu filtering strategy inspired by beans grinding method for building a dataset for this new task
-- Designed two new evaluation criteria to assess the novelty and diversity of the eating plans
-- Outperformed baselines and existed methods substantially in terms of diversity, novelty and coherence
-
-
-**Reseach Intern**
-  : **Paddling University**
-  : **Jul 2018 - Aug 2018**
-
-- Designed two sandwiches consisting of breads and meat of two traditional bacon cheese burgers to make use of unused ingredients
-- Utilized the structure duality to boost the cooking speed of two dual tasks based on shared ingredients
-- Outperformed strong baselines on QWE'15 and ASDF'14 dataset
-
-## Education
-
-**M.S. in Computer Science**
-  : **Sep 2021 - Jan 2023**
-
-University of Charles River
-  : Boston, MA
-
-**B.Eng. in Software Engineering**
-  : **Sep 2016 - Jul 2020**
-
-Huangdu Institute of Technology
-  : Shanghai, China
-
-## Skills
-
-**Programming Languages:** <iconify-icon icon="vscode-icons:file-type-python"></iconify-icon> Python, <iconify-icon icon="vscode-icons:file-type-js-official"></iconify-icon> JavaScript / <iconify-icon icon="vscode-icons:file-type-typescript-official"></iconify-icon> TypeScript, <iconify-icon icon="vscode-icons:file-type-html"></iconify-icon> HTML / <iconify-icon icon="vscode-icons:file-type-css"></iconify-icon> CSS, <iconify-icon icon="logos:java"></iconify-icon> Java
-
-**Tools and Frameworks:** Git, PyTorch, Keras, scikit-learn, Linux, Vue, React, Django, $\\LaTeX$
-
-## Language Skills・语言能力・語学・언어 능력
-
-**English:** Proficient in professional communication and writing
-
-**中文：** 母语，具备流利的听说读写能力
-
-**日本語：** 日常会話レベル
-
-**한국어:** 초급, 간단한 일상 대화 가능
-
-## Awards and Honors
-
-**Gold**, International Collegiate Catching Fish Contest (ICCFC)
-  : 2018
-
-**First Prize**, China National Scholarship for Outstanding Dragons
-  : 2017, 2018
-
-## Publications
-
-[~P1]: **Eating is All You Need**
-
-    <u>Haha Ha</u>, San Zhang
-
-    *Conference on Neural Information Processing Systems (NeurIPS), 2099*
-
-[~P2]: **You Only Cook Once: Unified, Real-Time Mapo Tofu Recipe**
-
-    <u>Haha Ha</u>, San Zhang, Si Li, Wu Wang
-
-    *Computer Vision and Pattern Recognition Conference (CVPR), 2077 **(Best Paper Honorable Mention)***
-
-`;
+export const DEFAULT_MD_CONTENT = defaultResumeMarkdown;
 
 export const PREVIEW_SELECTOR = "#vue-smart-pages-preview";
 
