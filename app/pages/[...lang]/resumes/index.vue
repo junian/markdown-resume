aa3e3cb733dd7643c15315ee217d2e572b1020ee
Junian Triajianto <author@junian.dev>
2026-08-13 16:00:04 +0700

    refactor: move resume card styles from index.vue to main.css for better organization

--- app/pages/[...lang]/resumes/index.vue
+++ app/pages/[...lang]/resumes/index.vue
@@ -51,25 +51,3 @@ const loadResumes = async () => {
 
 onMounted(loadResumes)
 </script>
-
-<style scoped>
-@reference "~/assets/css/main.css";
-
-.resumes-page :deep(.resume-card) {
-  border-color: #d1d5db !important;
-  color-scheme: light;
-}
-
-.resumes-page :deep(.resume-card .vue-smart-pages) {
-  background-color: white !important;
-  color: black !important;
-}
-
-.resumes-page :deep(button.resume-card) {
-  background-color: #e5e7eb !important;
-}
-
-.resumes-page :deep(button.resume-card:hover) {
-  background-color: white !important;
-}
-</style>
